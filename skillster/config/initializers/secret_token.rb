48079f8824620165bfba7d7a65c13271edeb6cd5
Joninna <jssimpsonsf@gmail.com>
2013-10-06 13:21:31 -0700

    Rails app

---
+++ skillster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skillster::Application.config.secret_token = '54d5bf1cdbe3fb49d308f7c95ba9a89a0cbc160f39c28594c3c0dde97f191ce6ae63c53858d4223590a9f48ffb103add5b2052f83185fedc2592c2d18f3ab488'
